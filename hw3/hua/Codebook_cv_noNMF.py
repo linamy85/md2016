b710fd3afb6b8f47fcb0a8e3b1eefe24ac8bdf56
MD2016 <md2016@github.com>
2016-12-18 04:10:25 +0000

    add codes

--- hw3/hua/Codebook_cv_noNMF.py
+++ hw3/hua/Codebook_cv_noNMF.py
@@ -5,7 +5,8 @@
 
 import pandas as pd
 import numpy as np
-import sys #from mf import matrix_factorization
+import sys
+#from mf import matrix_factorization
 import random
 from numpy.linalg import multi_dot
 import math
@@ -13,8 +14,8 @@ from sklearn.cluster import KMeans
 from scipy.cluster.vq import vq, kmeans
 
 
-if len(sys.argv) != 5:
-    print("usage: python3 CodebookTransfer.py [user_clusterN] [item_clusterN] [transfer_T] [dirName]")
+if len(sys.argv) != 4:
+    print("usage: python3 CodebookTransfer.py [user_clusterN] [item_clusterN] [transfer_T]")
     sys.exit(0)
 
 # In[2]:
@@ -37,8 +38,7 @@ transfer_T = [int(sys.argv[3]), 1000]
 interval = [10, 500]
 
 # source -> mf -> source
-dirname = sys.argv[4]
-filename = [[dirname+"/source_mf.txt", dirname+"/train.txt", dirname+"/test.txt"], ["test3/my/source.txt", "test3/my/train.txt", "test3/my/test.txt"]]
+filename = [["cv/source_mf.txt", "cv/train.txt", "cv/test.txt"], ["test3/my/source.txt", "test3/my/train.txt", "test3/my/test.txt"]]
 
 
 # ## Data processing
@@ -51,18 +51,11 @@ R1_mf = np.zeros((N1_r[TYPE], N1_c[TYPE]))
 W1 = np.zeros((N1_r[TYPE], N1_c[TYPE]))
 
 source_m = source.as_matrix()
-print(source_m.shape)
 
-if source_m.shape[0] == N1_r[TYPE] * N1_c[TYPE]:
-    for i in range(N1_r[TYPE]):
-        for j in range(N1_c[TYPE]):
-            user, item, rating = i, j, int(source_m[i*N1_r[TYPE]+j])
-            R1_mf[user-1][item-1] = rating
-            W1[user-1][item-1] = 1
-else:
-    for i in range(source_m.shape[0]):
-        user, item, rating = int(source_m[i][0]), int(source_m[i][1]), int(source_m[i][2])
-        R1[user-1][item-1] = rating
+for i in range(N1_r[TYPE]):
+    for j in range(N1_c[TYPE]):
+        user, item, rating = i, j, int(source_m[i*N1_r[TYPE]+j])
+        R1_mf[user-1][item-1] = rating
         W1[user-1][item-1] = 1
 
 print(R1_mf)
@@ -164,102 +157,30 @@ def codebook_construct(X, threshold, k, l):
     #print(S_old)
         
     
-    eta = 10   #any number > threshold
-    #t = 0
-    while eta > threshold:
-        up = multi_dot([np.transpose(X), U_old, S_old])
-        down = multi_dot([V_old, np.transpose(V_old), np.transpose(X), U_old, S_old])
-        V_new = np.sqrt(np.multiply(V_old, np.divide(up, down)))
-        
-        eta = squareSum(np.subtract(V_new, V_old))
-        
-        up = multi_dot([X, V_new, np.transpose(S_old)])
-        down = multi_dot([U_old, np.transpose(U_old), X, V_new, np.transpose(S_old)])
-        U_new = np.sqrt(np.multiply(U_old, np.divide(up, down)))
-        
-        eta += squareSum(np.subtract(U_new, U_old))
-        
-        up = multi_dot([np.transpose(U_new), X, V_new])
-        down = multi_dot([np.transpose(U_new), U_new, S_old, np.transpose(V_new), V_new])
-        S_new = np.sqrt(np.multiply(S_old, np.divide(up, down)))
-        
-        eta += squareSum(np.subtract(S_new, S_old))
-        eta /= 3
-        
-        V_old = V_new
-        U_old = U_new
-        S_old = S_new
-        #print(t, eta)
-        #t += 1
-        
-    '''
-    # check if U^TU=I, V^TV=I
-    if np.array_equal(np.dot(np.transpose(U_old), U_old), np.identity(n)) == False:
-        print('U^TU!=I')
-        print(np.dot(np.transpose(U_old), U_old))
-    
-    if np.array_equal(np.dot(np.transpose(V_old), V_old), np.identity(m)) == False:
-        print('V^TV!=I')
-        print(np.dot(np.transpose(V_old), V_old))
-    '''
-    
-    #print('U: ', U_new)
-    #print('V: ', V_new)
-    print('finish initialization')
-        
     U_aux = np.zeros(shape=(n, k))
     V_aux = np.zeros(shape=(m, l))
     
     
     for i in range(n):
-        j_max = np.argmax(U_new[i])
+        j_max = np.argmax(U_old[i])
         U_aux[i] = np.zeros(shape=(1, k))
         U_aux[i][j_max] = 1
         #print('U: ', i, j_max)
 
-    
-    # remove zeroCol
-    '''
-    zeroCol = []
-    for j in range(U_aux.shape[1]):
-        if(np.sum(U_aux[:, j]) == 0):
-            zeroCol.append(j)
-            
-    U_aux = np.delete(U_aux, zeroCol, axis=1)
-    '''
-    
     print('finish U_aux:\n', U_aux)
     
     
     for i in range(m):
-        j_max = np.argmax(V_new[i])
-        #print('V: ', i, j_max)
+        j_max = np.argmax(V_old[i])
         V_aux[i] = np.zeros(shape=(1, l))
         V_aux[i][j_max] = 1
 
-    #remove zeroCol
-    '''
-    zeroCol = []
-    for i in range(V_aux.shape[0]):
-        if(np.sum(V_aux[i]) == 0):
-            # remove col
-            zeroCol.append(i)
-            
-    V_aux = np.delete(V_aux, zeroCol, axis=0)
-    '''
-    #for i in range(V_aux.shape[0]):
-        #print(i, np.sum(V_aux[i]))
-            
     print('finish V_aux:\n', V_aux)
-    
-    
-    
     
     one = np.zeros(shape=(n, 1))
     one.fill(1)
     one2 = np.zeros(shape=(1, m))
     one2.fill(1)
-    #print(np.transpose(U_aux).shape, one.shape, one2.shape, V_aux.shape)
     up = multi_dot([np.transpose(U_aux), X, V_aux])
     down = multi_dot([np.transpose(U_aux), one, one2, V_aux])
     #print('up\n', up)
@@ -367,8 +288,6 @@ print('X_tgt_new\n', X_tgt_new)
 '''
 # open test.txt with r+ -> read -> seek0 -> truncate -> write
 with open(filename[TYPE][2], "r+") as f:
-    print('=============================================')
-    print('write file to: ', filename[TYPE][2])
     testline = f.readlines()
     f.seek(0, 0)
     f.truncate()
@@ -376,8 +295,6 @@ with open(filename[TYPE][2], "r+") as f:
         tmp = testline[i].split(" ")
         user, item = int(tmp[0]), int(tmp[1])
         f.write(tmp[0]+" "+tmp[1]+" "+str(X_tgt_new[user-1][item-1])+"\n")
-        #print(tmp[0]+" "+tmp[1]+" "+str(X_tgt_new[user-1][item-1])+"\n")
-    print('=============================================')
     f.close()
 '''
 
