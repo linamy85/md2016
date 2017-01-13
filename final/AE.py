c9ea8a37bb7c650e02341d740f768b4609ccd6e5
Amy Lin <linamy85@gmail.com>
2017-01-13 04:52:08 +0000

    correct output of dim_reduce

--- final/AE.py
+++ final/AE.py
@@ -9,95 +9,94 @@ def sigmoid(x):
 
 # Building the encoder
 def encoder(weights, biases, x):
-	# Encoder Hidden layer with sigmoid activation #1
-	layer = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['enc_w']), biases['enc_b']))
-	return layer
+        # Encoder Hidden layer with sigmoid activation #1
+        layer = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['enc_w']), biases['enc_b']))
+        return layer
 
 
 # Building the decoder
 def decoder(weights, biases, x):
-	# Encoder Hidden layer with sigmoid activation #1
-	layer = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['dec_w']), biases['dec_b']))
-	return layer
+        # Encoder Hidden layer with sigmoid activation #1
+        layer = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['dec_w']), biases['dec_b']))
+        return layer
 
 # HIDDEN_N
 def dim_reduce (data, HIDDEN_N, batch_size=256, training_epochs=20, learning_rate=0.01):
 
-	DATA_N = data.shape[0]
+        DATA_N = data.shape[0]
 
-	# Parameters
-	#learning_rate = 0.01
-	#training_epochs = 20
-	#batch_size = 256
-	display_step = 1
+        # Parameters
+        #learning_rate = 0.01
+        #training_epochs = 20
+        #batch_size = 256
+        display_step = 5
 
-	# Network Parameters
-	n_hidden_1 = HIDDEN_N # 1st layer feature_num
-	n_input = data.shape[1] # input data feature_num
+        # Network Parameters
+        n_hidden_1 = HIDDEN_N # 1st layer feature_num
+        n_input = data.shape[1] # input data feature_num
 
-	# tf Graph input (only pictures)
-	X = tf.placeholder("float", [None, n_input])
+        # tf Graph input (only pictures)
+        X = tf.placeholder("float", [None, n_input])
 
-	weights = {
-		'enc_w': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
-		'dec_w': tf.Variable(tf.random_normal([n_hidden_1, n_input])),
-	}
-	biases = {
-		'enc_b': tf.Variable(tf.random_normal([n_hidden_1])),
-		'dec_b': tf.Variable(tf.random_normal([n_input])),
-	}
-
-
-
-	# Construct model
-	encoder_op = encoder(weights, biases, X)
-	decoder_op = decoder(weights, biases, encoder_op)
-
-	# Prediction
-	y_pred = decoder_op
-	# Targets (Labels) are the input data.
-	y_true = X
-
-	# Define loss and optimizer, minimize the squared error
-	cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
-	optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
-
-	# Initializing the variables
-	init = tf.initialize_all_variables()
-
-	# Launch the graph
-	with tf.Session() as sess:
-		sess.run(init)
-		total_batch = int(DATA_N / batch_size)
-		print('start training')
-		# Training cycle
-		for epoch in range(training_epochs):
-			# Loop over all batches
-			for i in range(total_batch):
-				batch_xs = data[i*batch_size : (i+1)*batch_size]
-				# Run optimization op (backprop) and cost op (to get loss value)
-				_, c = sess.run([optimizer, cost], feed_dict={X: batch_xs})
-			# Display logs per epoch step
-			if epoch % display_step == 0:
-				print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c))
-
-		print("Optimization Finished!")
-
-		# Extract variables
-		w_list = sess.run(weights)
-		b_list = sess.run(biases)
+        weights = {
+                'enc_w': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+                'dec_w': tf.Variable(tf.random_normal([n_hidden_1, n_input])),
+        }
+        biases = {
+                'enc_b': tf.Variable(tf.random_normal([n_hidden_1])),
+                'dec_b': tf.Variable(tf.random_normal([n_input])),
+        }
 
 
-	# Forward to get X_FE
-	w = w_list['enc_w']
-	b = b_list['enc_b']
 
-	X_FE = []
-	total_batch = int(DATA_N/batch_size)
-	for i in range(total_batch):
-		batch_xs = data[i*batch_size : (i+1)*batch_size]
-		X_FE.append(sigmoid(np.add(np.dot(np.transpose(w), batch_xs[i]), b)))
-	X_FE = np.asarray(X_FE)
+        # Construct model
+        encoder_op = encoder(weights, biases, X)
+        decoder_op = decoder(weights, biases, encoder_op)
 
-	return X_FE
+        # Prediction
+        y_pred = decoder_op
+        # Targets (Labels) are the input data.
+        y_true = X
+
+        # Define loss and optimizer, minimize the squared error
+        cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
+        optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
+
+        # Initializing the variables
+        init = tf.global_variables_initializer()
+
+        # Launch the graph
+        with tf.Session() as sess:
+                sess.run(init)
+                total_batch = int(DATA_N / batch_size)
+                print('start training')
+                # Training cycle
+                for epoch in range(training_epochs):
+                        # Loop over all batches
+                        for i in range(total_batch):
+                                batch_xs = data[i*batch_size : (i+1)*batch_size]
+                                # Run optimization op (backprop) and cost op (to get loss value)
+                                _, c = sess.run([optimizer, cost], feed_dict={X: batch_xs})
+                        # Display logs per epoch step
+                        if epoch % display_step == 0:
+                                print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c))
+
+                print("Optimization Finished!")
+
+                # Extract variables
+                w_list = sess.run(weights)
+                b_list = sess.run(biases)
+
+
+        # Forward to get X_FE
+        w = w_list['enc_w']
+        b = b_list['enc_b']
+
+        X_FE = []
+        total_batch = int(DATA_N/batch_size)
+        for i in range(DATA_N):
+                X_FE.append(sigmoid(np.add(np.dot(np.transpose(w), data[i]), b)))
+        X_FE = np.asarray(X_FE)
+
+        return X_FE
 
--- final/DR_AE.py
+++ final/DR_AE.py
@@ -2,10 +2,27 @@ import AE
 
 import numpy as np
 
-# NOTE: data must be "normalized" before dim_reduce
-data = np.array([[1, 2, 3, 4, 5], [5, 2, 4, 1, 3], [1, 2, 3, 4, 5], [5, 2, 4, 1, 3]])
+data = np.array([[1, 2, 3, 4, 5, 1, 2, 3, 4, 5], [5, 2, 4, 1, 3, 5, 2, 4, 1, 3], [1, 2, 3, 4, 5, 1, 2, 3, 4, 5], [5, 2, 4, 1, 3, 5, 2, 4, 1, 3]])
+print('input:', data.shape[0], ',', data.shape[1])
+print(data)
+print('==============================')
 
-# API: dim_reduce (data, HIDDEN_N, batch_size=256, training_epochs=20, learning_rate=0.01):
+# API: dim_reduce (dataN, HIDDEN_N, batch_size=256, training_epochs=20, learning_rate=0.01):
+################### Training data ############################
+# data:         (vectorN, dim of each vector) must be "normalized" before dim_reduce
+# batch_size:   since vectorN (i.e.data.shape[0]) may be very large, we devides them into batches and train each batch.
+#               e.g. data: (256000, 999) and we set batch_size to 256, we train data of size (256, 999) each time
+#
+################### Parameters to adjust ############################
+# HIDDEN_N:             #neuron in the hidden layer
+# training_epochs:      iteration
+# learning_rate:        :)
+
+################### output ############################
+# data after dim_reduce: (vectorN, HIDDEN_N)
+
 X = AE.dim_reduce(data, 3, 2, 20, 0.01)
 
+print('==============================')
+print('answer:', X.shape[0], ',', X.shape[1])
 print(X)
